0a5521073f6ee4faf6fe2189787e1cefc27f8030
pankaj2186 <pansingh@adobe.com>
2025-06-13 11:43:35 +0530

    dm-template component rendering

--- blocks/dynamicmedia-template/dynamicmedia-template.js
+++ blocks/dynamicmedia-template/dynamicmedia-template.js
@@ -5,56 +5,58 @@
 export default function decorate(block) {
   console.log(block);
 
-  let deliveryType = Array.from(block.children)[2]?.textContent?.trim();
   let inputs = block.querySelectorAll('.dynamicmedia-template > div');
-  /*
-  let inputsArray = Array.from(inputs);
-  if(inputsArray.length < 2) {
-    console.log("Missing inputs, expecting 2, ensure both the image and DM URL are set in the dialog");
-    return;
-  }
-  // Get DM Url input
-  let dmUrlEl = inputs[3]?.getElementsByTagName("a")[0];
-  let rotate = inputs[4]?.textContent?.trim();
-  let flip = inputs[5]?.textContent?.trim();
+  
+  let configSrc = Array.from(block.children)[0]?.textContent?.trim(); //inline or cf
 
   
-        // Ensure S7 is loaded
-      if (typeof s7responsiveImage !== 'function') {
-        console.error("s7responsiveImage function is not defined, ensure script include is added to head tag");
-        return;
-      }
+  if(configSrc === 'inline'){
+    // Get DM Url input
+    let templateURL = inputs[1]?.textContent?.trim();
+    let variablemapping = inputs[2]?.textContent?.trim();
+
+    if(!templateURL) {
+      console.log("Missing mandatory template URL");
+      return;
+    }
+
+    if (variablemapping) {
+      // Step 1: Convert to key-value object
+      const paramPairs = variablemapping.split(',');
+      const paramObject = {};
+
+      paramPairs.forEach(pair => {
+        const [key, value] = pair.split('=');
+        if (key && value) {
+          paramObject[key.trim()] = decodeURIComponent(value.trim());
+        }
+      });
+
+      // Step 2: Append as query params to templateURL
+      const finaldmurl = new URL(templateURL);
+      Object.entries(paramObject).forEach(([key, value]) => {
+        finaldmurl.searchParams.append(key, value);
+      });
+
+      console.log("Final URL:", finaldmurl.toString());
+    }
+
+    const finalImg = document.createElement('img');
+    Object.assign(finalImg, {
+      className: 'dm-template-image',
+      src: finaldmurl.toString(),
+      alt: 'dm-template-image',
+    });
     
-      // Get image
-      let imageEl = inputs[1]?.getElementsByTagName("img")[0];
-      if(!imageEl) {
-        console.error("Image element not found, ensure it is defined in the dialog");
-        return;
-      }
+    block.append(finalImg);
+  } if(configSrc === 'cf'){
+
     
-      let imageSrc = imageEl.getAttribute("src");
-      if(!imageSrc) {
-        console.error("Image element source not found, ensure it is defined in the dialog");
-        return;
-      }
-    
-      // Get imageName from imageSrc expected in the format /content/dam/<...>/<imageName>.<extension>
-      let imageName = imageSrc.split("/").pop().split(".")[0];
-    
-        let dmUrl = dmUrlEl.getAttribute("href");
-      
-        imageEl.setAttribute("data-src", dmUrl + (dmUrl.endsWith('/') ? "" : "/") + imageName);
-        //imageEl.setAttribute("src", dmUrl + (dmUrl.endsWith('/') ? "" : "/") + imageName);
-        imageEl.setAttribute("src", dmUrl + (dmUrl.endsWith('/') ? "" : "/") + imageName);
-        imageEl.setAttribute("data-mode", "smartcrop");
-      
-        //s7responsiveImage(imageEl);
-        //dmUrlEl.remove();
- 
-  block.children[6]?.remove();
-  block.children[5]?.remove();
-  block.children[4]?.remove();
+   
+  }
+  block.children[0]?.remove();
+  block.children[1]?.remove();
+  block.children[2]?.remove();
   block.children[3]?.remove();
-  block.children[2]?.remove();  
-   */
+  block.children[4]?.remove();  
 }
