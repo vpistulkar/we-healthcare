300e1edace272c3c91a66e875d51bd0763aee26d
pankaj2186 <pansingh@adobe.com>
2025-06-23 16:56:48 +0530

    Update dynamicmedia-image.js

--- blocks/dynamicmedia-image/dynamicmedia-image.js
+++ blocks/dynamicmedia-image/dynamicmedia-image.js
@@ -14,51 +14,54 @@ export default function decorate(block) {
   }
   // Get DM Url input
   let altText = inputs[6].textContent?.trim();
-  let dmUrlEl = inputs[2]?.getElementsByTagName("a")[0];
+  let dmUrlEl = inputs[2]?.getElementsByTagName("a")[0]  || "https://smartimaging.scene7.com/is/image/DynamicMediaNA";
   let rotate = inputs[4]?.textContent?.trim();
   let flip = inputs[5]?.textContent?.trim();
-  
-  if(deliveryType === 'dm' && dmUrlEl){
-      // Ensure S7 is loaded
-      if (typeof s7responsiveImage !== 'function') {
-        console.error("s7responsiveImage function is not defined, ensure script include is added to head tag");
-        return;
+  if(deliveryType != "na"){  
+      if(deliveryType === 'dm' && dmUrlEl){
+          // Ensure S7 is loaded
+          if (typeof s7responsiveImage !== 'function') {
+            console.error("s7responsiveImage function is not defined, ensure script include is added to head tag");
+            return;
+          }
+        
+          // Get image
+          let imageEl = inputs[1]?.getElementsByTagName("img")[0];
+          if(!imageEl) {
+            console.error("Image element not found, ensure it is defined in the dialog");
+            return;
+          }
+        
+          let imageSrc = imageEl.getAttribute("src");
+          if(!imageSrc) {
+            console.error("Image element source not found, ensure it is defined in the dialog");
+            return;
+          }
+        
+          // Get imageName from imageSrc expected in the format /content/dam/<...>/<imageName>.<extension>
+          let imageName = imageSrc.split("/").pop().split(".")[0];
+        
+          if(!dmUrlEl) {
+            console.error("DM Url not found, make sure its set in the dialog");
+          }else{
+            let dmUrl = dmUrlEl.getAttribute("href");
+          
+            imageEl.setAttribute("data-src", dmUrl + (dmUrl.endsWith('/') ? "" : "/") + imageName);
+            //imageEl.setAttribute("src", dmUrl + (dmUrl.endsWith('/') ? "" : "/") + imageName);
+            imageEl.setAttribute("src", dmUrl + (dmUrl.endsWith('/') ? "" : "/") + imageName);
+            imageEl.setAttribute("alt", altText ? altText : 'dynamic media image');
+            imageEl.setAttribute("data-mode", "smartcrop");
+          
+            s7responsiveImage(imageEl);
+            dmUrlEl.remove();
+          }
       }
-    
-      // Get image
-      let imageEl = inputs[1]?.getElementsByTagName("img")[0];
-      if(!imageEl) {
-        console.error("Image element not found, ensure it is defined in the dialog");
-        return;
+      if(deliveryType === 'dm-openapi'){
+        
+       
       }
-    
-      let imageSrc = imageEl.getAttribute("src");
-      if(!imageSrc) {
-        console.error("Image element source not found, ensure it is defined in the dialog");
-        return;
-      }
-    
-      // Get imageName from imageSrc expected in the format /content/dam/<...>/<imageName>.<extension>
-      let imageName = imageSrc.split("/").pop().split(".")[0];
-    
-      if(!dmUrlEl) {
-        console.error("DM Url not found, make sure its set in the dialog");
-      }else{
-        let dmUrl = dmUrlEl.getAttribute("href");
-      
-        imageEl.setAttribute("data-src", dmUrl + (dmUrl.endsWith('/') ? "" : "/") + imageName);
-        //imageEl.setAttribute("src", dmUrl + (dmUrl.endsWith('/') ? "" : "/") + imageName);
-        imageEl.setAttribute("src", dmUrl + (dmUrl.endsWith('/') ? "" : "/") + imageName);
-        imageEl.setAttribute("alt", altText ? altText : 'dynamic media image');
-        imageEl.setAttribute("data-mode", "smartcrop");
-      
-        s7responsiveImage(imageEl);
-        dmUrlEl.remove();
-      }
-  }
-  if(deliveryType === 'dm-openapi'){
-    
-   
+  }else{
+    //remove everthing from here.
   }
   /*
   block.children[6]?.remove();
