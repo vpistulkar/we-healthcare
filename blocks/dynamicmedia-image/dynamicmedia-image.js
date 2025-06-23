e5b1fefccfbf2941328e7ed5f12e709b04562455
pankaj2186 <pansingh@adobe.com>
2025-06-23 16:51:35 +0530

    Update dynamicmedia-image.js

--- blocks/dynamicmedia-image/dynamicmedia-image.js
+++ blocks/dynamicmedia-image/dynamicmedia-image.js
@@ -14,7 +14,7 @@ export default function decorate(block) {
   }
   // Get DM Url input
   let altText = inputs[6].textContent?.trim();
-  let dmUrlEl = inputs[3]?.getElementsByTagName("a")[0];
+  let dmUrlEl = inputs[2]?.getElementsByTagName("a")[0];
   let rotate = inputs[4]?.textContent?.trim();
   let flip = inputs[5]?.textContent?.trim();
   
