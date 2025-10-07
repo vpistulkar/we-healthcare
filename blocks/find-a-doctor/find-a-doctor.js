bee6252c6818f45407a08a1d1715a17e7530c9dc
Vrushali Pistulkar <vpistulkar@adobe.com>
2025-10-07 15:30:32 +0530

    test graphql
    test graphql

--- blocks/find-a-doctor/find-a-doctor.js
+++ blocks/find-a-doctor/find-a-doctor.js
@@ -3,7 +3,8 @@ import { getMetadata } from '../../scripts/aem.js';
 import { isAuthorEnvironment } from '../../scripts/scripts.js';
 
 // Sample doctor data - in production, this would come from your data source
-const GRAPHQL_DOCTORS_BY_FOLDER_QUERY = '/graphql/execute.json/weHealthcare/GetDoctorsFromFolder';
+const GRAPHQL_DOCTORS_BY_FOLDER_QUERY = '/graphql/execute.json/ref-demo-eds/GetDoctorsFromFolder';
+ 
 const CONFIG = {
   /*WRAPPER_SERVICE_URL: 'https://defaultfa7b1b5a7b34438794aed2c178dece.e1.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/2660b7afa9524acbae379074ae38501e/triggers/manual/paths/invoke',*/
    /*WRAPPER_SERVICE_PARAMS: 'api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=540k5P2jXO3u7i_9VxBp_X3aTqTn8a0o9zYjQiJt5pQ'*/
